54ae44ef3153baeb8a27332d8fdab91b63a619fe
Xavian Moody <XavianMoody1025@gmail.com>
2025-04-08 16:22:39 -0400

    Created server folder

---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import { server } from "./src/mocks/node.js";
+import mongoose from "mongoose";
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+
+// Server
+app.listen(process.env.PORT, () => {
+  console.log(`Server has started on port ${process.env.PORT}`);
+
+  // Connect database (Mongodb)
+  mongoose
+    .connect("mongodb://localhost:27017/")
+    .then(() => console.log("Database Connected"))
+    .catch((error) => {
+      console.log(error);
+    });
+
+  server.listen(); // (Mock Service Worker) for testing API responses
+});
